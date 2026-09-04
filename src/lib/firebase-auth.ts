20c4511e2b6d4c5aceb6bf1388224d86a48c7720
BIZMATE <smirtoil2015@gmail.com>
2026-09-04 16:44:24 +0300

    Remove Firebase auth dependency from BIZMATE

--- src/lib/firebase-auth.ts
+++
@@ -1,9 +0,0 @@
-import { getAuth, type Auth } from 'firebase/auth';
-import { getFirebaseApp } from '@/lib/firebase-config';
-
-let auth: Auth | undefined;
-
-export function getFirebaseAuth() {
-  if (!auth) auth = getAuth(getFirebaseApp());
-  return auth;
-}
